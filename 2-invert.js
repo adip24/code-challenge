069c95f02780260c37a9d383aa046286c3772800
Adip Safiudin <adipsafiudin@Adips-MacBook-Air.local>
2024-11-03 17:34:54 +0700

    2-invert

---
+++ 2-invert.js
@@ -0,0 +1,9 @@
+// Code Challenge 3 November 2024
+// By Codewars
+// https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
+
+const invert = (array) => {
+  return array.map((arr) => -arr)
+}
+
+console.log(invert([1, 2, 3, 4, 5]))
