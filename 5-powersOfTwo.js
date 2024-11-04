6f33f2caa00a1e69dca2a09f065c3bb297afa83f
Adip Safiudin <adipsafiudin@Adips-MacBook-Air.local>
2024-11-04 18:22:27 +0700

    5-powersOfTwo

---
+++ 5-powersOfTwo.js
@@ -0,0 +1,13 @@
+// Code Challenge 3 November 2024
+// By Codewars
+// https://www.codewars.com/kata/57a083a57cb1f31db7000028
+
+const powersOfTwo = (n) => {
+  var array = []
+  for (let index = 0; index <= n; index++) {
+    array.push(Math.pow(2, index))
+  }
+  return array
+}
+
+console.log(powersOfTwo(3))
