7f5e62a39f706ca7fea3b08f8fee8358026ba7f6
Adip Safiudin <adipsafiudin@Adips-MacBook-Air.local>
2024-11-04 18:47:06 +0700

    7-unusualFive

---
+++ 7-unusualFive.js
@@ -0,0 +1,9 @@
+// Code Challenge 4 November 2024
+// By Codewars
+// https://www.codewars.com/kata/59441520102eaa25260000bf
+
+const unusualFive = () => {
+  return 'hmmmm'.length
+}
+
+console.log(unusualFive())
