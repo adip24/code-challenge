c1f5fcd67aba1344fbe0eed979ebbf3e7eba77c5
Adip Safiudin <adipsafiudin@Adips-MacBook-Air.local>
2024-11-04 18:53:31 +0700

    8-getChar

---
+++ 8-getChar.js
@@ -0,0 +1,9 @@
+// Code Challenge 4 November 2024
+// By Codewars
+// https://www.codewars.com/kata/55ad04714f0b468e8200001c
+
+const getChar = (c) => {
+  return String.fromCharCode(c)
+}
+
+console.log(getChar(55))
