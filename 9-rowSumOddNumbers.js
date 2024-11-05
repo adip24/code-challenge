22f68865156fccd3b5f9d8f0c28c7e20ba11d71e
Adip Safiudin <adipsafiudin@Adips-MacBook-Air.local>
2024-11-05 08:22:19 +0700

    9-rowSumOddNumbers

---
+++ 9-rowSumOddNumbers.js
@@ -0,0 +1,9 @@
+// Code Challenge 5 November 2024
+// By Codewars
+// https://www.codewars.com/kata/55fd2d567d94ac3bc9000064
+
+const rowSumOddNumbers = (n) => {
+  return Math.pow(n, 3)
+}
+
+console.log(rowSumOddNumbers(42))
