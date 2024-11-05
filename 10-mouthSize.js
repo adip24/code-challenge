9253c16edeed9bfde1b35877fdaa861c31f05c5c
Adip Safiudin <adipsafiudin@Adips-MacBook-Air.local>
2024-11-05 08:27:23 +0700

    10-mouthSize

---
+++ 10-mouthSize.js
@@ -0,0 +1,9 @@
+// Code Challenge 5 November 2024
+// By Codewars
+// https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+const mouthSize = (animal) => {
+  return animal.toLowerCase() == 'alligator' ? 'small' : 'wide'
+}
+
+console.log(mouthSize('alligator'))
