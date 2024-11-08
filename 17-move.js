f574137407c4795df4d478d319198108b471c88c
Adip Safiudin <adip240499@gmail.com>
2024-11-08 20:04:44 +0700

    17 move

---
+++ 17-move.js
@@ -0,0 +1,7 @@
+// Code Challenge 7 November 2024
+// By Codewars
+// https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/
+
+const move = (position, roll) => position + roll * 2
+
+console.log(move(2, 5))
