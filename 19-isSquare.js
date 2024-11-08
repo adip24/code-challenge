c8d04a920c02170e46ba6681827ce557af681f6f
Adip Safiudin <adip240499@gmail.com>
2024-11-08 20:31:14 +0700

    19 isSquar

---
+++ 19-isSquare.js
@@ -0,0 +1,7 @@
+// Code Challenge 8 November 2024
+// By Codewars
+// https://www.codewars.com/kata/54c27a33fb7da0db0100040e/
+
+const isSquare = (n) => Math.sqrt(n) % 1 == 0
+
+console.log(isSquare(4))
